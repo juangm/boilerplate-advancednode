2d1ef47762332325ea956d12eed7f158514a89d1
juangm <juangarciamarinblog@gmail.com>
2020-08-14 14:08:16 +0200

    Add mongoDB

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require("express-session");
 const passport = require("passport");
 const app = express();
 const ObjectID = require("mongodb").ObjectID;
+const mongo = require("mongodb").MongoClient;
 
 // Set PUG
 app.set('view engine', 'pug');
@@ -21,15 +22,24 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {
-  done(null, null)
-  // done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-  // db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  //   done(null, doc);
-  // });
-  done(null, null)
+// Connect to Mongo
+mongo.connect(process.env.MONGO_URI, (err, db) => {
+  if (err) {
+    console.log("Database error: " + err);
+  } else {
+    console.log("Successful database connection");
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    passport.deserializeUser((id, done) => {
+      db.collection("users").findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, doc);
+      });
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
 });
 
 fccTesting(app); //For FCC testing purposes
@@ -41,8 +51,4 @@ app.route("/").get((req, res) => {
   //Change the response to render the Pug template
   res.render('pug/index', {title: 'Hello', message: 'Please login'} )
   // res.send(`Pug template is not defined.`);
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
 });
