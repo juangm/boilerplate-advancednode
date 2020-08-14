927139c284c97f6777e5e7b5ab871843b97d3fd4
juangm <juangarciamarinblog@gmail.com>
2020-08-14 13:50:39 +0200

    Adding passport and session

--- server.js
+++ server.js
@@ -2,11 +2,23 @@
 
 const express = require("express");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const session = require("express-session");
+const passport = require("passport");
 const app = express();
 
 // Set PUG
 app.set('view engine', 'pug');
+
+// Session
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'randomString',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -15,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('pug/index.pug')
+  res.render('pug/index', {title: 'Hello', message: 'Please login'} )
   // res.send(`Pug template is not defined.`);
 });
 
